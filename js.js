b6ed020d6dc23330f8ba112e0dc3bd88a171dd4c
Nandana Krishna <162736802+nandanakrishna25@users.noreply.github.com>
2025-02-11 10:56:03 +0530

    Update js.js

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ let fName="Krishna"
 var age=12,c;
 fName="Nandana"
 const height=165
-c=5;
+c=3;
 b=c/age;
 console.log(age)
 
@@ -39,4 +39,57 @@ console.log(Student);
 
 window.alert("Click OK");
 
- 
+if (age= 15) 
+    console.log("Age is 15");
+else if(age>15)
+    console.log("Age greater than 15");
+else
+console.log("age less than 15");
+
+switch (c) {
+    case 1:
+        fName = "Neha";
+        break;
+    case 2:
+        fName = "Mia";
+        break;
+    case 3:
+        fName = "Nandana";
+        break;
+    default:
+        dayName = "Invalid";
+}
+console.log(fName);
+
+for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0) continue;
+    console.log(i);
+}
+for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0) break;
+    console.log(i);
+}
+
+let obj = {
+    name: "Sourav",
+    age: 20,
+    job: "Student"
+};
+console.log(obj);
+
+let obj2 = new Object();
+obj.name= "Sanvi",
+obj.age= 27,
+obj.job= "Teacher"
+
+console.log(obj);
+
+console.log(obj.name);
+console.log(obj["age"]);
+obj.age = 18;
+console.log(obj);
+let obj = { City: "Tvm" };
+console.log(obj)
+delete obj.age;
+console.log("color" in obj);
+console.log(obj.hasOwnProperty("City"));
