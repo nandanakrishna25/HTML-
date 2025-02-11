dc5b9155b19fa55c6eb54d31959249908d23322c
Nandana Krishna <nandana2564@gmail.com>
2025-02-11 08:00:22 +0530

    js

--- js.js
+++ js.js
@@ -1,14 +1,42 @@
-let fName="Nandana"
-let age=12,c;
-
+//variables
+let fName="Krishna"
+var age=12,c;
+fName="Nandana"
+const height=165
 c=5;
 b=c/age;
+console.log(age)
 
-function newFn(){
+//functions
+function newFn(age){
     console.log(fName)
     age=20
-    console.log(age)
+    const places="tvm"
+    console.log(age+"\n" +"name:"+fName)
 }
-
 newFn()
 console.log(age)
+// console.log(place)
+
+//datatypes
+let i;
+console.log(i); 
+let empty = null;
+console.log(empty); 
+let sym = Symbol('unique');
+console.log(sym); 
+
+let places= ["GFG", "Computer", "Hello"];
+console.log(places);
+
+let Student = {
+    firstName: "Johnny",
+    lastName: "Diaz", 
+    age: 35, 
+    mark: "blueEYE"
+}
+console.log(Student);
+
+window.alert("Click OK");
+
+ 
