8d3da518b08fdd007eeeb80d98859354d78a9367
Nandana Krishna <nandana2564@gmail.com>
2025-02-12 06:49:56 +0530

    new

--- js.js
+++ js.js
@@ -1,42 +1,64 @@
-//variables
-let fName="Krishna"
-var age=12,c;
-fName="Nandana"
-const height=165
-c=5;
-b=c/age;
-console.log(age)
+console.log(b)
+let a=10
+var b=20
+const c=30
+console.log(a,b,c)
 
-//functions
-function newFn(age){
-    console.log(fName)
-    age=20
-    const places="tvm"
-    console.log(age+"\n" +"name:"+fName)
-}
-newFn()
-console.log(age)
-// console.log(place)
+// console.log("hi")
 
-//datatypes
-let i;
-console.log(i); 
-let empty = null;
-console.log(empty); 
-let sym = Symbol('unique');
-console.log(sym); 
+// function a1(){
+//     var age=10
+//     console.log(age)
+//     let v=2
+//     let a=3
+//     const y=4
+// }
+// a1()
+// console.log(age)
 
-let places= ["GFG", "Computer", "Hello"];
-console.log(places);
+// // //variables
+// // "use strict"
+// // let fName="Krishna"
+// // console.log(age)
+// // var age=12,c;
+// // fName="Nandana"
+// // const height=165
+// // var age=10
+// // console.log(age)
+// // c=5;
+// // b=c/age;
+// // console.log(age)
 
-let Student = {
-    firstName: "Johnny",
-    lastName: "Diaz", 
-    age: 35, 
-    mark: "blueEYE"
-}
-console.log(Student);
+// // //functions
+// // function newFn(age){
+// //     console.log(fName)
+// //     age=20
+// //     const places="tvm"
+// //     console.log(age+"\n" +"name:"+fName)
+// // }
+// // newFn()
+// // console.log(age)
+// // // console.log(place)
 
-window.alert("Click OK");
+// // //datatypes
+// // let i;
+// // console.log(i); 
+// // let empty = null;
+// // console.log(empty); 
+// // let sym = Symbol('unique');
+// // console.log(sym); 
+
+// // let places= ["GFG", "Computer", "Hello"];
+// // console.log(places);
+
+// // let Student = {
+// //     firstName: "Johnny",
+// //     lastName: "Diaz", 
+// //     age: 35, 
+// //     mark: "blueEYE"
+// // }
+// // console.log(Student);
+
+// // window.alert("Click OK");
 
  
